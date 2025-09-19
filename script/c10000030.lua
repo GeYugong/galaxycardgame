2eb918a07d45b0c5662796919b9f1325fd8a3a11
whenmo <91626218+whenmo@users.noreply.github.com>
2025-09-19 13:36:32 +0800

    fix  10000030 sp condition

--- script/c10000030.lua
+++ script/c10000030.lua
@@ -8,14 +8,14 @@ function s.initial(c)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	--这张卡在自己补给8以上时才可以特殊召唤
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(s.splimit)
-	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetCondition(s.con2)
+	c:RegisterEffect(e2)
 end
---额外条件：补给必须≥8
-function s.splimit(e,se,sp,st)
-	return Duel.GetSupply(e:GetHandlerPlayer()) >= 8
+--额外条件：补给必须 >= 8
+function s.con2(e)
+	return Duel.GetSupply(e:GetHandlerPlayer()) < 8
 end
